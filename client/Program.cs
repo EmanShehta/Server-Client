c083fbfd5d0bee24c32a3671b0d21f22457bd93c
EmanShehta <e3036768@gmail.com>
2022-04-21 04:33:34 +0200

    Network

---
+++ client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress host_id = IPAddress.Parse("127.0.0.1");  // Socket type== protocoltype  --- tcp   udp
+            IPEndPoint Endpointhost = new IPEndPoint(host_id, 8000);
+            Socket SOCK = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            SOCK.Connect(Endpointhost);
+            byte[] DataforClient = new byte[1024 * 5000];
+            string Path_receivied = "E:/Assignment/server/bin/Debug/netcoreapp3.1";
+            int receivedBytesLen = SOCK.Receive(DataforClient);
+            int Length_file_name = BitConverter.ToInt32(DataforClient, 0);
+            string name_file = Encoding.ASCII.GetString(DataforClient, 4, Length_file_name);
+            string the_content = Encoding.ASCII.GetString(DataforClient, 4 + Length_file_name, 10000);
+            Console.WriteLine(name_file);
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("The File has been received successfully.");
+            Console.WriteLine("--------------------------------------------------ConTentFile-----------------------------------------------------------");
+            Console.WriteLine(the_content);
+            SOCK.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEndpoint = new IPEndPoint(IPAddress.Any, 8000);
+            Socket server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server_socket.Bind(ipEndpoint);
+            server_socket.Listen(100);
+            Socket sock_client = server_socket.Accept();
+            string Path_file_location = @"E:\Assignment\server\bin\Debug\netcoreapp3.1";
+            string Name_file_text = "Testing.txt";
+            byte[] Name_file_server = Encoding.ASCII.GetBytes(Name_file_text);
+            byte[] Data_in_file = File.ReadAllBytes(Name_file_text);
+            byte[] Data_client = new byte[4 + Name_file_server.Length + Data_in_file.Length];
+            byte[] length_file_name = BitConverter.GetBytes(Name_file_server.Length);
+            length_file_name.CopyTo(Data_client, 0);   //from 0 
+            Name_file_server.CopyTo(Data_client, 4);    // from 4
+            Data_in_file.CopyTo(Data_client, 4 + Name_file_server.Length); /// from 4 + file  name length
+            sock_client.Send(Data_client);
+            Console.WriteLine(Path_file_location + Name_file_text);
+            Console.WriteLine(Encoding.ASCII.GetString(Data_in_file));
+            Console.WriteLine("The File has been sent successfully.");
+            Console.WriteLine("Operation Done .");
+            Console.WriteLine("Eman shehta abd alaziz");
+            Console.WriteLine("Computer science");
+            Console.WriteLine("Section 1");
+            sock_client.Shutdown(SocketShutdown.Both);
+            sock_client.Close();
+            Console.ReadLine();
+        }
+    }
+}
+
